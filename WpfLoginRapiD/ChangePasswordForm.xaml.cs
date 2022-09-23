311e911f509bd1ebed5b8d1b155a03bc42412caf
co2mott <matthijswietsma@gmail.com>
2022-09-23 10:45:03 +0200

    Add project files.

---
+++ WpfLoginRapiD/ChangePasswordForm.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WpfLoginRapiD
+{
+    /// <summary>
+    /// Interaction logic for ChangePasswordForm.xaml
+    /// </summary>
+    public partial class ChangePasswordForm : Window
+    {
+        public ChangePasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                SqlConnection con = new SqlConnection("Data Source=LAPTOP-IS69AVIT\\SQLEXPRESS;Initial Catalog=Rapid;Integrated Security=True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Update loginrapid set password=@password where ID = @ID", con);
+
+                cmd.Parameters.AddWithValue("@ID", int.Parse(TxtID.Text));
+                cmd.Parameters.AddWithValue("@password", TxtChangePassword.Text);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Succsesfully Updated");
+            }
+        }
+
+        private void TxtCurrentPassword_Leave(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TxtCurrentPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfLoginRapiD/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfLoginRapiD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=LAPTOP-IS69AVIT\\SQLEXPRESS;Initial Catalog=Rapid;Integrated Security=True;");
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txt_Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_login_Click(object sender, RoutedEventArgs e)
+        {
+            String username, user_password;
+
+            username = txt_Username.Text;
+            user_password = txt_Password.Text;
+
+            try
+            {
+                String querry = "SELECT * FROM loginrapid WHERE username = '" + txt_Username.Text + "' AND password = '" + txt_Password.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = txt_Username.Text;
+                    user_password = txt_Password.Text;
+
+
+                    MainForm form2 = new MainForm();
+                    form2.Show();
+                    this.Hide();
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details", "Error");
+                    txt_Username.Clear();
+                    txt_Password.Clear();
+
+                    txt_Username.Focus();
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button_clear_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Username.Clear();
+            txt_Password.Clear();
+
+            txt_Username.Focus();
+        }
+
+        private void button_exit_Click(object sender, RoutedEventArgs e)
+        {
+
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePasswordForm form3 = new ChangePasswordForm();
+            form3.Show();
+            this.Hide();
+        }
+    }
+}
